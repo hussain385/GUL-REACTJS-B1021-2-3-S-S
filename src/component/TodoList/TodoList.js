fb6c35af818e4ec71e7c68c591e4d6335b98d850
Hussain Saifuddin <hussainquetta53@gmail.com>
2021-11-26 18:44:22 +0500

    completed the app

--- src/component/TodoList/TodoList.js
+++ src/component/TodoList/TodoList.js
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import "./todoList.css";
-// import db from "../Firebase/firebase.config";
-// import { setDoc, collection, getDocs } from "firebase/firestore/lite";
+import db from "../Firebase/firebase.config";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore/lite";
 
 const TodoList = ({ text, id }) => {
+  const [showEdit, setShowEdit] = useState(false);
+  const [editTodo, setEditTodo] = useState("");
+  const delTodo = async () => {
+    await deleteDoc(doc(db, "todos", id)).then(() => {
+      alert("Your File Has Successfully Delete");
+      window.location.reload();
+    });
+  };
+
+  const editTodoFunction = async () => {
+    await updateDoc(doc(db, "todos", id), {
+      todo: editTodo,
+    }).then(() => {
+      alert("Your Todo Has Successfully Updated");
+      window.location.reload();
+    });
+  };
   return (
-    <div className="todoListStyle">
-      <p style={{ marginTop: "7px" }}>{text}</p>
-      <div className="features">
-        <button className="featureStyle" style={{ marginRight: "2.5px" }}>
-          <i class="far fa-trash-alt"></i>
-        </button>
-        <button
-          className="featureStyle"
-          style={{ marginLeft: "2.5px", backgroundColor: "darkkhaki" }}
-        >
-          <i class="far fa-edit"></i>
-        </button>
+    <>
+      <div className="todoListStyle">
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <p style={{ marginTop: "7px" }}>{text}</p>
+          <div className="features">
+            <button
+              onClick={delTodo}
+              className="featureStyle"
+              style={{ marginRight: "2.5px" }}
+            >
+              <i className="far fa-trash-alt"></i>
+            </button>
+            <button
+              onClick={() => {
+                setShowEdit(!showEdit);
+                setEditTodo(text);
+              }}
+              className="featureStyle"
+              style={{ marginLeft: "2.5px", backgroundColor: "darkkhaki" }}
+            >
+              <i className="far fa-edit"></i>
+            </button>
+          </div>
+        </div>
+        {showEdit && (
+          <div style={{ flexDirection: "row"}}>
+            <input
+              value={editTodo}
+              onChange={({ target }) => {
+                setEditTodo(target.value);
+              }}
+              placeholder="Enter Your Todo"
+              className="inputStyle1"
+            />
+            <button class="editStyle" onClick={editTodoFunction}>
+              <i class="fas fa-check"></i>
+            </button>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
